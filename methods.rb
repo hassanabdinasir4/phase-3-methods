7d903780878f38e0a36440f5bcbaf4985e925730
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2023-02-08 10:55:53 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,50 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name ="Naureen")
+    puts "Hello, #{name}!"
+end
+
+
+def greet (name ="Jimmy")
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name ="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+
+def add num1, num2
+    puts num1
+    puts num2
+    num1 + num2
+end
+
+def halve(num1=7)
+    if num1.class != Integer
+        return nil
+    end
+    num1 / 2
+    end
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
